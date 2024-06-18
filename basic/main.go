f27c5cbded06629581cd6976a8dcf6c75f29ae7e
Satoshi Watanabe <sassy.watanabe@gmail.com>
2024-06-18 10:49:56 +0900

    named return value

--- basic/main.go
+++ basic/main.go
@@ -17,9 +17,16 @@ func swap(x, y string) (string, string) {
 	return y, x
 }
 
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
 func main() {
 	fmt.Println(add(42, 13))
 	fmt.Printf("Now you have %g problems.\n", sqrt(7))
 	a, b := swap("world", "hello")
 	fmt.Println(a, b)
+	fmt.Println(split(17))
 }
