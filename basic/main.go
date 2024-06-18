5716d49c2152ba59abe2bfbc7311c5429801f280
Satoshi Watanabe <sassy.watanabe@gmail.com>
2024-06-18 10:46:14 +0900

    関数は複数の戻り値を返セル

--- basic/main.go
+++ basic/main.go
@@ -13,7 +13,13 @@ func sqrt(x int) float64 {
 	return math.Sqrt(float64(x))
 }
 
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
 func main() {
 	fmt.Println(add(42, 13))
 	fmt.Printf("Now you have %g problems.\n", sqrt(7))
+	a, b := swap("world", "hello")
+	fmt.Println(a, b)
 }
