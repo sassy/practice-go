814ffd07d5a88420574fcc2e02ad8c738045baa2
Satoshi Watanabe <sassy.watanabe@gmail.com>
2024-06-18 17:56:43 +0900

    変数について追加

--- basic/main.go
+++ basic/main.go
@@ -23,7 +23,12 @@ func split(sum int) (x, y int) {
 	return
 }
 
+var i, j int = 1, 2
+
 func main() {
+	k := 3
+	var c, python, java = true, false, "no!"
+	fmt.Println(i, j, k, c, python, java)
 	fmt.Println(add(42, 13))
 	fmt.Printf("Now you have %g problems.\n", sqrt(7))
 	a, b := swap("world", "hello")
